a30be427f3770fe8fcef3833565e4a822d849f02
desen-1318 <sukamdestu@gmail.com>
2022-06-05 19:23:03 +0700

    baru

---
+++ lib/atribut/warna.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class warna extends StatelessWidget {
+  const warna({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
